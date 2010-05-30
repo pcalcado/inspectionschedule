29e8bd9b2573d7a5d6d323bb1ae3e1d04b76db7d
Phillip Calcado <pcalcado@gmail.com>
2010-05-30 18:04:31 +1000

    property now has coordinates

--- InspectionScheduleIpad/Classes/InspectionScheduleIpadProperty.h
+++ InspectionScheduleIpad/Classes/InspectionScheduleIpadProperty.h
@@ -1,13 +1,7 @@
-//
-//  Property.h
-//  InspectionScheduleIpad
-//
-//  Created by Caue Guerra on 30/05/10.
-//  Copyright 2010 ThoughtWorks. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
-
+#import <MapKit/MapKit.h>
+#import <MapKit/MKAnnotation.h>
+#import <MapKit/MKReverseGeocoder.h>
 
 @interface InspectionScheduleIpadProperty : NSObject {
 	NSString *address;
@@ -17,10 +11,12 @@
 	NSInteger *bathroom;
 	NSInteger *carspace;
 	NSString *description;
+	CLLocationCoordinate2D coordinate;
 }
 
 @property(nonatomic, copy) NSString *address;
 @property(nonatomic, copy) NSString *type;
 @property(nonatomic, copy) NSString *description;
+@property(nonatomic, assign) CLLocationCoordinate2D coordinate;
 
 @end
--- InspectionScheduleIpad/Classes/InspectionScheduleIpadProperty.m
+++ InspectionScheduleIpad/Classes/InspectionScheduleIpadProperty.m
@@ -1,11 +1,3 @@
-//
-//  Property.m
-//  InspectionScheduleIpad
-//
-//  Created by Caue Guerra on 30/05/10.
-//  Copyright 2010 ThoughtWorks. All rights reserved.
-//
-
 #import "InspectionScheduleIpadProperty.h"
 
 
@@ -14,6 +6,7 @@
 @synthesize address;
 @synthesize type;
 @synthesize description;
+@synthesize coordinate;
 
 - (void) dealloc {
 	self.address = nil;
