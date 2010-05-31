2f3db20dec5655b7587ad59376ee493aa93c56ce
Caue Guerra <caueguerra@gmail.com>
2010-05-31 23:48:02 +1000

    using split view

--- InspectionSchedulerIpad/Classes/RootViewController.h
+++ InspectionSchedulerIpad/Classes/RootViewController.h
@@ -10,7 +10,7 @@
 
 @class DetailViewController;
 
-@interface RootViewController : UITableViewController {
+@interface RootViewController : UIViewController <UIPopoverControllerDelegate, UISplitViewControllerDelegate> {
     DetailViewController *detailViewController;
 }
 
