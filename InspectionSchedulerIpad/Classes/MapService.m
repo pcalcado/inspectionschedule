bcde87afd08ddec3a8fc8a900a399ab8a7665c5a
Phillip Calcado <pcalcado@gmail.com>
2010-06-14 15:41:25 +1000

    phil: hardcoded manly coordinates

--- InspectionSchedulerIpad/Classes/MapService.m
+++ InspectionSchedulerIpad/Classes/MapService.m
@@ -20,21 +20,21 @@
 
 - (CLLocationCoordinate2D)coordinatesForLocation:(NSString *)location  {
 	CLLocationCoordinate2D coordinate;
-	NSString *googleApiUrl = [NSString stringWithFormat:@"http://maps.google.com/maps/api/geocode/json?sensor=false&region=au&address=%@", location];
-	NSLog(googleApiUrl);
-	NSURL *url = [NSURL URLWithString:googleApiUrl];
+	//	NSString *googleApiUrl = [NSString stringWithFormat:@"http://maps.google.com/maps/api/geocode/json?sensor=false&region=au&address=%@", location];
+	//	NSLog(googleApiUrl);
+	//	NSURL *url = [NSURL URLWithString:googleApiUrl];
 
-	NSDictionary *result = [[webService responseBodyForUrl:url] JSONValue];
-	NSString *status = [result valueForKey:@"status"];
-	if (![status isEqualToString:@"OK"]) {
+	//	NSDictionary *result = [[webService responseBodyForUrl:url] JSONValue];
+	//	NSString *status = [result valueForKey:@"status"];
+	//	if (![status isEqualToString:@"OK"]) {
 	  //TODO: put erro handling back 
-	  NSLog([NSString stringWithFormat:@"Error retrieving coordinates for location [%@]: Google maps API returned status [%@]", location, status]);
-		return coordinate;
-	}
+	//	  NSLog([NSString stringWithFormat:@"Error retrieving coordinates for location [%@]: Google maps API returned status [%@]", location, status]);
+	//		return coordinate;
+	//	}
 	
-	NSDictionary *resultLocation = [[result valueForKeyPath:@"results.geometry.location"] objectAtIndex:0];
-	coordinate.latitude = [[resultLocation valueForKey:@"lat"] doubleValue];
-	coordinate.longitude = [[resultLocation valueForKey:@"lng"] doubleValue];
+	//	NSDictionary *resultLocation = [[result valueForKeyPath:@"results.geometry.location"] objectAtIndex:0];
+	coordinate.latitude =  -33.7971440;
+	coordinate.longitude = 151.2880400;
 	return coordinate;
 }
 
