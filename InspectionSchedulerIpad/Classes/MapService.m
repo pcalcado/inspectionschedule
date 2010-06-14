53279e214c37a6432bd8e97f6214af29b48fd373
Phillip Calcado <pcalcado@gmail.com>
2010-06-14 15:46:02 +1000

    removed tests and unused stuff

--- InspectionSchedulerIpad/Classes/MapService.m
+++ InspectionSchedulerIpad/Classes/MapService.m
@@ -2,7 +2,6 @@
 #import "NSString+Extensions.h"
 #import "NSString+SBJSON.h"
 #import "WebService.h"
-#import "Error.h"
 
 @implementation MapService
 
@@ -20,19 +19,6 @@
 
 - (CLLocationCoordinate2D)coordinatesForLocation:(NSString *)location  {
 	CLLocationCoordinate2D coordinate;
-	//	NSString *googleApiUrl = [NSString stringWithFormat:@"http://maps.google.com/maps/api/geocode/json?sensor=false&region=au&address=%@", location];
-	//	NSLog(googleApiUrl);
-	//	NSURL *url = [NSURL URLWithString:googleApiUrl];
-
-	//	NSDictionary *result = [[webService responseBodyForUrl:url] JSONValue];
-	//	NSString *status = [result valueForKey:@"status"];
-	//	if (![status isEqualToString:@"OK"]) {
-	  //TODO: put erro handling back 
-	//	  NSLog([NSString stringWithFormat:@"Error retrieving coordinates for location [%@]: Google maps API returned status [%@]", location, status]);
-	//		return coordinate;
-	//	}
-	
-	//	NSDictionary *resultLocation = [[result valueForKeyPath:@"results.geometry.location"] objectAtIndex:0];
 	coordinate.latitude =  -33.7971440;
 	coordinate.longitude = 151.2880400;
 	return coordinate;
--- InspectionSchedulerIpad/Tests/UnitTest.h
+++
@@ -1,4 +0,0 @@
-#import "GTMSenTestCase.h"
-#define HC_SHORTHAND
-#import <OCHamcrest/OCHamcrest.h>
-#import <OCMock/OCMock.h>
