432c2975151a5b48d386c5687fccc557d81ec1e7
Phillip Calcado <pcalcado@gmail.com>
2010-06-15 22:47:18 +1000

    phil: formatting

--- InspectionSchedulerIpad/Classes/DetailViewController.m
+++ InspectionSchedulerIpad/Classes/DetailViewController.m
@@ -44,7 +44,7 @@
 
 - (IBAction)map {
   NSLog(@"a %a", mapViewController);
-	[self presentModalViewController:mapViewController animated:YES];
+  [self presentModalViewController:mapViewController animated:YES];
 }
 
 /*
@@ -52,15 +52,15 @@
  */
 - (void)setDetailItem:(id)newDetailItem {
     if (detailItem != newDetailItem) {
-        [detailItem release];
-        detailItem = [newDetailItem retain];
+      [detailItem release];
+      detailItem = [newDetailItem retain];
         
-        // Update the view.
-        [self configureView];
+      // Update the view.
+      [self configureView];
     }
 
     if (popoverController != nil) {
-        [popoverController dismissPopoverAnimated:YES];
+      [popoverController dismissPopoverAnimated:YES];
     }        
 }
 
@@ -70,20 +70,17 @@
     detailDescriptionLabel.text = [detailItem description];   
 }
 
-- (void)splitViewController: (UISplitViewController*)svc willHideViewController:(UIViewController *)aViewController withBarButtonItem:(UIBarButtonItem*)barButtonItem forPopoverController: (UIPopoverController*)pc {
-    
-    barButtonItem.title = @"Root List";
-    NSMutableArray *items = [[toolbar items] mutableCopy];
-    [items insertObject:barButtonItem atIndex:0];
-    [toolbar setItems:items animated:YES];
-    [items release];
-    self.popoverController = pc;
+- (void)splitViewController: (UISplitViewController*)svc willHideViewController:(UIViewController *)aViewController withBarButtonItem:(UIBarButtonItem*)barButtonItem forPopoverController: (UIPopoverController*)pc {    
+  barButtonItem.title = @"Root List";
+  NSMutableArray *items = [[toolbar items] mutableCopy];
+  [items insertObject:barButtonItem atIndex:0];
+  [toolbar setItems:items animated:YES];
+  [items release];
+  self.popoverController = pc;
 }
 
-
 // Called when the view is shown again in the split view, invalidating the button and popover controller.
-- (void)splitViewController: (UISplitViewController*)svc willShowViewController:(UIViewController *)aViewController invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem {
-    
+- (void)splitViewController: (UISplitViewController*)svc willShowViewController:(UIViewController *)aViewController invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem {    
     NSMutableArray *items = [[toolbar items] mutableCopy];
     [items removeObjectAtIndex:0];
     [toolbar setItems:items animated:YES];
@@ -97,45 +94,44 @@
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-	return 1;
+  return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-	return [propertiesArray count];
+  return [propertiesArray count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-	static NSString *CellIdentifier = @"Cell"; 
-	UITableViewCell *cell =
-    [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-	if (cell == nil) {
-		[[NSBundle mainBundle] loadNibNamed:@"InspectionTableCellView" owner:self options:NULL];
-		cell = nibLoadedCell;
-	}
-	Property *property = [propertiesArray objectAtIndex:indexPath.row];
-	UILabel *addressLabel = (UILabel*) [cell viewWithTag:1];
-	addressLabel.text = property.address;
-	UILabel *typeLabel = (UILabel*) [cell viewWithTag:2];
-	typeLabel.text = property.type;
-	UILabel *descriptionLabel = (UILabel*) [cell viewWithTag:3];
-	descriptionLabel.text = property.description;
-	return cell;
+  static NSString *CellIdentifier = @"Cell"; 
+  UITableViewCell *cell =  [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+  if (cell == nil) {
+    [[NSBundle mainBundle] loadNibNamed:@"InspectionTableCellView" owner:self options:NULL];
+    cell = nibLoadedCell;
+  }
+  Property *property = [propertiesArray objectAtIndex:indexPath.row];
+  UILabel *addressLabel = (UILabel*) [cell viewWithTag:1];
+  addressLabel.text = property.address;
+  UILabel *typeLabel = (UILabel*) [cell viewWithTag:2];
+  typeLabel.text = property.type;
+  UILabel *descriptionLabel = (UILabel*) [cell viewWithTag:3];
+  descriptionLabel.text = property.description;
+  return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-	UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
-	if (cell.accessoryType == UITableViewCellAccessoryCheckmark) {
-		cell.accessoryType = UITableViewCellAccessoryNone;
-		[selectedPropertiesArray removeObject: [propertiesArray objectAtIndex:indexPath.row]];	  
-	} else {
-		cell.accessoryType = UITableViewCellAccessoryCheckmark;
-		[selectedPropertiesArray addObject: [propertiesArray objectAtIndex:indexPath.row]];
-	}
+  UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+  if (cell.accessoryType == UITableViewCellAccessoryCheckmark) {
+    cell.accessoryType = UITableViewCellAccessoryNone;
+    [selectedPropertiesArray removeObject: [propertiesArray objectAtIndex:indexPath.row]];	  
+  } else {
+    cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    [selectedPropertiesArray addObject: [propertiesArray objectAtIndex:indexPath.row]];
+  }
 }
 
 - (void)viewDidUnload {
-    self.popoverController = nil;
+  self.popoverController = nil;
 }
 
 @end
